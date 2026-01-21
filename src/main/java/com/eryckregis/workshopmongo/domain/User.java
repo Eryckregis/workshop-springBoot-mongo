f3e907b6cb7041603ac1170b5f0862c0ef12c967
eryck regis da silva <eryckengsoftware@gmail.com>
2026-01-21 16:49:37 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/eryckregis/workshopmongo/domain/User.java
@@ -0,0 +1,59 @@
+package com.eryckregis.workshopmongo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+
+    }
+
+    public User(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
