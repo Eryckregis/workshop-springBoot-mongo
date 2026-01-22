a3b11e42dd963610820f9b6f78ff1550f6d15115
eryck regis da silva <eryckengsoftware@gmail.com>
2026-01-22 18:01:26 -0300

    Inserção de usuário com POST

--- src/main/java/com/eryckregis/workshopmongo/services/UserService.java
+++ src/main/java/com/eryckregis/workshopmongo/services/UserService.java
@@ -1,11 +1,14 @@
 package com.eryckregis.workshopmongo.services;
 
 import com.eryckregis.workshopmongo.domain.User;
+import com.eryckregis.workshopmongo.dto.UserDTO;
 import com.eryckregis.workshopmongo.repositories.UserRepository;
 import com.eryckregis.workshopmongo.services.exception.objectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +29,13 @@ public List<User> findAll(){
         return obj.orElseThrow(() -> new objectNotFoundException("Objeto não encontrado"));
 }
 
+@PostMapping
+public User insert(User obj){
+    return userRepository.insert(obj);
+}
 
+public User fromDTO(UserDTO objDto){
+    return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+}
 
 }
